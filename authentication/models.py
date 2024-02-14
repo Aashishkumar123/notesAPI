7b553ffda03571c5e390bdfee68b7ebafe934fc8
Aashish <aashishkumar12376@gmail.com>
2024-02-14 11:38:16 +0530

    [CREATE] profile model.

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    class Gender(models.Choices):
+        MALE = "Male"
+        FEMALE = "Female"
+        OTHERS = "Others"
+
+    user = models.OneToOneField(User, verbose_name="User", on_delete=models.CASCADE)
+    image = models.ImageField(
+        verbose_name="User Image", upload_to="profile", default="default.png"
+    )
+    gender = models.CharField(
+        max_length=100, verbose_name="Gender", choices=Gender.choices
+    )
+
+    def __str__(self):
+        return str(self.user)
